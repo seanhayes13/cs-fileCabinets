c15a2f0ab0d26fe21a472613176e8c2ed1e827bd
Sean Hayes <seanhayes13@hotmail.com>
2024-01-24 07:29:29 -0700

    202401240729 - Init

---
+++ Utility/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace fileCabinets.Utility
+{
+    public class Constants
+    {
+        public static SolidColorBrush SUCCESS_MSG_COLOR = new SolidColorBrush(Colors.Black);
+
+        public static SolidColorBrush FAIL_MSG_COLOR = new SolidColorBrush(Colors.Red);
+    }
+}
